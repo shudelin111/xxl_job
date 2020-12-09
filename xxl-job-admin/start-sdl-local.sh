190c8a21ff620513ff138381f3d1635ff4d92390
shudl <749087934@qq.com>
2020-12-09 21:35:43 +0800

    2020年12月09日 修改配置文件

---
+++ xxl-job-admin/start-sdl-local.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+cd `dirname $0`
+
+img_mvn="maven:3.3.3-jdk-8"                 # docker image of maven
+m2_cache=/user/local/apache-maven-3.6.3/repository                 # the local maven cache dir
+proj_home=$PWD                              # the project root dir
+img_output="commons/xxl-job"         # output image tag
+
+git pull  # should use git clone https://name:pwd@xxx.git
+
+echo "use docker maven"
+docker run --rm \
+   -v $m2_cache:/user/local/apache-maven-3.6.3/repository \
+   -v $proj_home:/usr/local/apache-maven-3.6.3 \
+   -w /usr/local/apache-maven-3.6.3 $img_mvn mvn clean package -U -Dmaven.test.skip=true
+
+sudo mv $proj_home/target/xxl-job-admin-*.jar $proj_home/app.jar # 兼容所有sh脚本
+docker build -t $img_output .
+
+mkdir -p $PWD/logs
+chmod 777 $PWD/logs
+
+# 删除容器
+docker rm -f xxl-job &> /dev/null
+
+version=`date "+%Y%m%d%H"`
+
+# 启动镜像
+docker run -d --restart=on-failure:5 --privileged=true \
+    --net=host \
+    -w /home \
+    -v $PWD/logs:/home/logs \
+    --name xxl-job commons/xxl-job \
+
+docker logs -f xxl-job
