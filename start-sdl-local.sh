fc16a0a3af66926674568a77b27ac10bfdff0867
shudl <749087934@qq.com>
2020-12-11 18:05:16 +0800

    调整配置文件

--- start-sdl-local.sh
+++ start-sdl-local.sh
@@ -27,7 +27,7 @@ version=`date "+%Y%m%d%H"`
 
 # 启动镜像
 docker run -d --restart=on-failure:5 --privileged=true \
-    --net=host \
+  #  --net=host \
     -w /usr/local/work \
     -v $PWD/doc/logs:/usr/local/work/doc/logs \
     --name xxl-job -p 30120:30120 commons/xxl-job \
